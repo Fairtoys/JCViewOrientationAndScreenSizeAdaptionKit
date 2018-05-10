4325cf7b5a4164fa290e5cb182a284f9260e3860
wangjunchao-hj@huajiao.tv <wangjunchao-hj@huajiao.tv>
2018-05-10 11:44:06 +0800

    Initial Commit

---
+++ JCViewOrientationAndScreenSizeAdaptionKit/JCViewOrientationAndScreenSizeAdaptionKit.h
@@ -0,0 +1,19 @@
+//
+//  JCViewOrientationAndScreenSizeAdaptionKit.h
+//  JCViewOrientationAndScreenSizeAdaptionKit
+//
+//  Created by huajiao on 2018/5/10.
+//  Copyright © 2018年 Cerko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JCViewOrientationAndScreenSizeAdaptionKit.
+FOUNDATION_EXPORT double JCViewOrientationAndScreenSizeAdaptionKitVersionNumber;
+
+//! Project version string for JCViewOrientationAndScreenSizeAdaptionKit.
+FOUNDATION_EXPORT const unsigned char JCViewOrientationAndScreenSizeAdaptionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCViewOrientationAndScreenSizeAdaptionKit/PublicHeader.h>
+
+
